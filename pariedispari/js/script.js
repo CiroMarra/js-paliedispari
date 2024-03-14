43e8aab4dfabe65712cad9a630eba9198ecb78c6
CiroMarra <tensha93@gmail.com>
2024-03-14 16:32:02 +0100

    feat: add a script for pariedispari

--- pariedispari/js/script.js
+++ pariedispari/js/script.js
@@ -4,3 +4,46 @@
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
+
+
+// chiedere pari e dispari a l'utente 
+// chiedere un numero a l'utente che vada da 1  a 5 
+// generare un numero random per il computer che vada da 1 a 5
+// sommare i due numeri
+// determinare se un numero è pari o dispari
+// in base a la scelta dell'utente dichiarare se ha vinto o ha perso.
+
+const userChoice = prompt('scegli pari o dispari').toLowerCase();
+const userNumber = parseInt(prompt('scegli un numero da 1 a 5'));
+const computerNumber = getRndInteger (1, 5);
+const playerVsPngSum = userNumber + computerNumber;
+let win;
+let showMessage;
+
+startGame();
+
+
+// FUNCTION
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function startGame(){
+    if(playerVsPngSum % 2 === 0){
+        win = 'pari';
+    }  else {
+        win = 'dispari';
+    }
+    
+    
+    const forUser = "è uscito " + playerVsPngSum;
+
+    if (userChoice === win) {
+        showMessage= ' hai vinto';
+    }   else {
+        showMessage= ' hai perso';
+    }
+    
+     alert(forUser+showMessage);
+}
