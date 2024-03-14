fd9ca4c1dfa3c00ecc8ce1cd3d114aebab5e2438
CiroMarra <tensha93@gmail.com>
2024-03-14 17:43:38 +0100

    feat: add script for palindrome

---
+++ palindromo/js/script.js
@@ -0,0 +1,44 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// Su internet ci sono soluzioni con join e reverse, non potete usarle
+
+// Chiedo a l'utente di inserire una parola.
+
+// const userWord = prompt('dimmi una parola');
+//     console.log(userWord);
+
+
+// // FUNCTION
+
+// function palindrome(word) {
+//     let revertWord = "";
+//     for(let i = userWord.length -1; i >= 0; i--) {
+//         thisLetter += userWord[i];
+//     }
+
+//     console.log(revertWord);
+//     return revertWord;
+// }
+let userWord = prompt('dimmi una parola');
+const revWord = revertWord(userWord);
+alert(revertWord(userWord));
+
+function revertWord(word) {
+    let revertedWord = '';
+    for(let i = word.length -1; i >=0; i--) {
+        const thisLetter = word[i];
+
+        revertedWord += thisLetter;
+    }
+
+    let result;
+    if (word === revertedWord) {
+        result = `la parola ${word}  è palindroma`;
+    }   else {
+        result = `la parola ${word} non è palindroma`;
+    }
+
+    return result;
+
+
+}
